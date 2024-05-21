c4934cda1077dcf2f357643ca0f1a3ba6a10e1cd
Sarah Divento <5ufkefu@gmail.com>
2024-05-21 20:33:54 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-#/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/cbusillo/BD_to_AVP/release/installer.sh)"
+#/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/5UFKEFU/BD_to_AVP/release/installer.sh)"
 
 handle_error() {
     echo "Error: $1"
